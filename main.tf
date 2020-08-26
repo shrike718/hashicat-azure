5db2f69ba54faa75102e682265f6677c9c4f1058
shrike718 <56272683+shrike718@users.noreply.github.com>
2020-08-26 14:54:31 -0400

    Update main.tf

--- main.tf
+++ main.tf
@@ -93,8 +93,10 @@ resource "azurerm_virtual_machine" "catapp" {
 
   network_interface_ids         = [azurerm_network_interface.catapp-nic.id]
   delete_os_disk_on_termination = "true"
-  Billable            = true
-  Department          = "devops"
+   tags = {
+    "Billable" = "true"
+     "Department"          = "devops"
+          }
   
   storage_image_reference {
     publisher = var.image_publisher
